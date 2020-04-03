c7d8c8d5b795b273b4ea56c753a842f01de80823
Motistrings <timgrand4929@gmail.com>
2020-04-03 11:33:28 +0300

    Initialized variables for calculation of day and akan names for both male and female arrays

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+var maleKwasiNames = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame"
+];
+var femaleKwasiNames = [
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua",
+  "Ama"
+];
+var Century, Year, Month, Day, d, dayResult;
